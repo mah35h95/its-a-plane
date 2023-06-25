3bf25dda8385ef40b75a25c1ae87c310be82ecf8
Mahesh G <47917543+mah35h95@users.noreply.github.com>
2023-06-25 23:02:34 +0530

    display and scroll

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from machine import Pin, SPI
 from ssd1306 import SSD1306_SPI
 import framebuf
@@ -12,6 +13,13 @@ except ImportError:
     print("Secrets including geo are kept in code_secrets.py, please add them there!")
     raise
 
+# Time in seconds to wait between scrolling one line and the next
+PAUSE_BETWEEN_LINE_SCROLLING = 3
+
+# How often to query fr24 - quick enough to catch a plane flying over, not so often as to cause any issues, hopefully
+QUERY_DELAY = 25
+
+# Area to search for flights, see secrets file
 # BOUNDS_BOX = "51.6,51.4,-0.3,-0.1"
 BOUNDS_BOX = secrets["bounds_box"]
 # area to search for flights: top latitude, bottom latitude, left longitude, right longitude (so this example is central London)
@@ -34,11 +42,12 @@ rheaders = {
 # Look for flights overhead
 def get_flights():
     try:
-        # response = json.loads(matrixportal.fetch())
         response = urequests.get(url=FLIGHT_SEARCH_URL, headers=rheaders).json()
-        print(response)
-    except:
-        print("Error during fetch")
+    except Exception as e:
+        print("Error during get")
+        print(e.__class__.__name__ + "----------------ERROR---------------")
+        print(e)
+        checkConnection()
         return False
     if len(response) == 3:
         # print ("Flight found.")
@@ -49,6 +58,205 @@ def get_flights():
                     return flight_id
     else:
         return False
+
+
+# Take the flight ID we found with a search, and load details about it
+def get_flight_details(fn):
+    # the JSON from FR24 is too big for the matrixportal memory to handle. So we load it in chunks into our static array,
+    # as far as the big "trails" section of waypoints at the end of it, then ignore most of that part. Should be about 9KB, we have 14K before we run out of room..
+    global json_bytes
+    global json_size
+    byte_counter = 0
+    chunk_length = 1024
+
+    # zero out any old data in the byte array
+    for i in range(0, json_size):
+        json_bytes[i] = 0
+
+    # Get the URL response one chunk at a time
+    try:
+        response = urequests.get(url=FLIGHT_LONG_DETAILS_HEAD + fn, headers=rheaders)
+        for chunk in response.iter_content(chunk_size=chunk_length):
+            # if the chunk will fit in the byte array, add it
+            if byte_counter + chunk_length <= json_size:
+                for i in range(0, len(chunk)):
+                    json_bytes[i + byte_counter] = chunk[i]
+            else:
+                print("Exceeded max string size while parsing JSON")
+                return False
+
+            # check if this chunk contains the "trail:" tag which is the last bit we care about
+            trail_start = json_bytes.find((b'"trail":'))
+            byte_counter += len(chunk)
+
+            # if it does, find the first/most recent of the many trail entries, giving us things like speed and heading
+            if not trail_start == -1:
+                # work out the location of the first } character after the "trail:" tag, giving us the first entry
+                trail_end = json_bytes[trail_start:].find((b"}"))
+                if not trail_end == -1:
+                    trail_end += trail_start
+                    # characters to add to make the whole JSON object valid, since we're cutting off the end
+                    closing_bytes = b"}]}"
+                    for i in range(0, len(closing_bytes)):
+                        json_bytes[trail_end + i] = closing_bytes[i]
+                    # zero out the rest
+                    for i in range(trail_end + 3, json_size):
+                        json_bytes[i] = 0
+                    # print(json_bytes.decode('utf-8'))
+
+                    # Stop reading chunks
+                    print("Details lookup saved " + str(trail_end) + " bytes.")
+                    return True
+    # Handle occasional URL fetching errors
+    except Exception as e:
+        print(e.__class__.__name__ + "----------------ERROR---------------")
+        print(e)
+        return False
+
+    # If we got here we got through all the JSON without finding the right trail entries
+    print("Failed to find a valid trail entry in JSON")
+    return False
+
+
+# Look at the byte array that fetch_details saved into and extract any fields we want
+def parse_details_json():
+    global json_bytes
+
+    try:
+        # get the JSON from the bytes
+        long_json = json.loads(json_bytes.decode("utf8"))
+
+        # Some available values from the JSON. Put the details URL and a flight ID in your browser and have a look for more.
+
+        flight_number = long_json["identification"]["number"]["default"]
+        # print(flight_number)
+        flight_callsign = long_json["identification"]["callsign"]
+        aircraft_code = long_json["aircraft"]["model"]["code"]
+        aircraft_model = long_json["aircraft"]["model"]["text"]
+        # aircraft_registration=long_json["aircraft"]["registration"]
+        airline_name = long_json["airline"]["name"]
+        # airline_short=long_json["airline"]["short"]
+        airport_origin_name = long_json["airport"]["origin"]["name"]
+        airport_origin_name = airport_origin_name.replace(" Airport", "")
+        airport_origin_code = long_json["airport"]["origin"]["code"]["iata"]
+        # airport_origin_country=long_json["airport"]["origin"]["position"]["country"]["name"]
+        # airport_origin_country_code=long_json["airport"]["origin"]["position"]["country"]["code"]
+        # airport_origin_city=long_json["airport"]["origin"]["position"]["region"]["city"]
+        # airport_origin_terminal=long_json["airport"]["origin"]["info"]["terminal"]
+        airport_destination_name = long_json["airport"]["destination"]["name"]
+        airport_destination_name = airport_destination_name.replace(" Airport", "")
+        airport_destination_code = long_json["airport"]["destination"]["code"]["iata"]
+        # airport_destination_country=long_json["airport"]["destination"]["position"]["country"]["name"]
+        # airport_destination_country_code=long_json["airport"]["destination"]["position"]["country"]["code"]
+        # airport_destination_city=long_json["airport"]["destination"]["position"]["region"]["city"]
+        # airport_destination_terminal=long_json["airport"]["destination"]["info"]["terminal"]
+        # time_scheduled_departure=long_json["time"]["scheduled"]["departure"]
+        # time_real_departure=long_json["time"]["real"]["departure"]
+        # time_scheduled_arrival=long_json["time"]["scheduled"]["arrival"]
+        # time_estimated_arrival=long_json["time"]["estimated"]["arrival"]
+        # latitude=long_json["trail"][0]["lat"]
+        # longitude=long_json["trail"][0]["lng"]
+        # altitude=long_json["trail"][0]["alt"]
+        # speed=long_json["trail"][0]["spd"]
+        # heading=long_json["trail"][0]["hd"]
+
+        if flight_number:
+            print("Flight is called " + flight_number)
+        elif flight_callsign:
+            print("No flight number, callsign is " + flight_callsign)
+        else:
+            print("No number or callsign for this flight.")
+
+        # Set up to 6 of the values above as text for display_flights to put on the screen
+        # Short strings get placed on screen, then longer ones scroll over each in sequence
+
+        global line1_short, line1_long, line2_short, line2_long, line3_short, line3_long
+
+        line1_short = flight_number
+        line1_long = airline_name
+        line2_short = airport_origin_code + "-" + airport_destination_code
+        line2_long = airport_origin_name + "-" + airport_destination_name
+        line3_short = aircraft_code
+        line3_long = aircraft_model
+
+        if not line1_short:
+            line1_short = ""
+        if not line1_long:
+            line1_long = ""
+        if not line2_short:
+            line2_short = ""
+        if not line2_long:
+            line2_long = ""
+        if not line3_short:
+            line3_short = ""
+        if not line3_long:
+            line3_long = ""
+
+        # optional filter example - check things and return false if you want
+
+        # if altitude > 10000:
+        #    print("Altitude Filter matched so don't display anything")
+        #    return False
+
+    except (KeyError, ValueError, TypeError) as e:
+        print("JSON error")
+        print(e)
+        return False
+
+    return True
+
+
+# Populate the lines, then scroll longer versions of the text
+def display_flight(oled):
+    global line1_short, line1_long, line2_short, line2_long, line3_short, line3_long, line1, line2, line3
+
+    line1 = line1_short
+    line2 = line2_short
+    line3 = line3_short
+    display_details(oled, line1, line2, line3)
+    sleep(PAUSE_BETWEEN_LINE_SCROLLING)
+
+    line1 = line1_long
+    display_details(oled, line1, line2, line3)
+    sleep(PAUSE_BETWEEN_LINE_SCROLLING)
+    line1 = line1_short
+
+    line2 = line2_long
+    display_details(oled, line1, line2, line3)
+    sleep(PAUSE_BETWEEN_LINE_SCROLLING)
+    line2 = line2_short
+
+    line3 = line3_long
+    display_details(oled, line1, line2, line3)
+    sleep(PAUSE_BETWEEN_LINE_SCROLLING)
+    line3 = line3_short
+
+    display_details(oled, line1, line2, line3)
+
+
+def display_details(oled, line1, line2, line3):
+    oled.text(line1, 0, 0)
+    oled.text(line2, 0, 16)
+    oled.text(line3, 0, 16 * 2)
+    sleep(2)
+    oled.show()
+    if len(line1) > 16:
+        scroll(oled, line1, 0, 0)
+    if len(line2) > 16:
+        scroll(oled, line2, 0, 16)
+    if len(line3) > 16:
+        scroll(oled, line3, 0, 16 * 2)
+
+
+def scroll(oled, line, x, y):
+    for i in range(160 + len(line) * 8):
+        oled.fill_rect(x, y, 128, 16, 0)
+        oled.text(line, 128 - x - i, y)
+        oled.show()
+        sleep_ms(10)
+    oled.fill_rect(x, y, 128, 16, 0)
+    oled.text(line, x, y)
+    oled.show()
 
 
 def display_logo(oled):
@@ -114,9 +322,33 @@ def checkConnection():
         print(f"Failed to connect. Status: {wlan.status()}")
 
 
+# Blank the display when a flight is no longer found
+def clear_flight(oled):
+    oled.fill(0)
+    oled.show()
+    global line1, line2, line3
+    line1 = line2 = line3 = ""
+
+
 # Connect to network
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
+
+# Some memory shenanigans - the matrixportal doesn't do great at assigning big strings dynamically. So we create a big static array to put the JSON results in each time.
+json_size = 14336
+json_bytes = bytearray(json_size)
+
+# text strings to go in the lines
+line1_short = ""
+line1_long = ""
+line2_short = ""
+line2_long = ""
+line3_short = ""
+line3_long = ""
+
+line1 = ""
+line2 = ""
+line3 = ""
 
 spi = SPI(0, 100000, mosi=Pin(19), sck=Pin(18))
 # oled = SSD1306_SPI(WIDTH, HEIGHT, spi, dc,rst, cs) use GPIO PIN NUMBERS
@@ -133,9 +365,40 @@ checkConnection()
 display_pikachu(oled)
 sleep(2)
 
-print(get_flights())
-
 display_plane(oled)
+line1_short = "some"
+line1_long = "some thing very long yeah"
+line2_short = "how"
+line2_long = "How does this even work?"
+line3_short = "yup"
+line3_long = "yup i totally get what is happening"
+display_flight(oled)
 
-# oled.fill(0)
-# oled.show()
+last_flight = ""
+flight_id = get_flights()
+
+if flight_id:
+    if flight_id == last_flight:
+        print("Same flight found, so keep showing it")
+    else:
+        print("New flight " + flight_id + " found, clear display")
+        clear_flight(oled)
+        if get_flight_details(flight_id):
+            if parse_details_json():
+                display_plane(oled)
+                display_flight(oled)
+            else:
+                print("error parsing JSON, skip displaying this flight")
+        else:
+            print("error loading details, skip displaying this flight")
+
+        last_flight = flight_id
+else:
+    print("No flights found, clear display")
+    clear_flight(oled)
+
+# sleep(5)
+
+
+# for i in range(0, QUERY_DELAY, +5):
+#     sleep(5)
