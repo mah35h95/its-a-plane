1e1aec14db62cc6ac104b91261385b99eaddee13
Mahesh G <47917543+mah35h95@users.noreply.github.com>
2023-06-25 20:52:12 +0530

    request flight radar

--- main.py
+++ main.py
@@ -12,6 +12,44 @@ except ImportError:
     print("Secrets including geo are kept in code_secrets.py, please add them there!")
     raise
 
+# BOUNDS_BOX = "51.6,51.4,-0.3,-0.1"
+BOUNDS_BOX = secrets["bounds_box"]
+# area to search for flights: top latitude, bottom latitude, left longitude, right longitude (so this example is central London)
+
+# URLs
+FLIGHT_SEARCH_HEAD = "https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds="
+FLIGHT_SEARCH_TAIL = "&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=0&estimated=0&maxage=14400&gliders=0&stats=0&ems=1&limit=1"
+FLIGHT_SEARCH_URL = FLIGHT_SEARCH_HEAD + BOUNDS_BOX + FLIGHT_SEARCH_TAIL
+# Used to get more flight details with a fr24 flight ID from the initial search
+FLIGHT_LONG_DETAILS_HEAD = "https://data-live.flightradar24.com/clickhandler/?flight="
+
+# Request headers
+rheaders = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+    "cache-control": "no-store, no-cache, must-revalidate, post-check=0, pre-check=0",
+    "accept": "application/json",
+}
+
+
+# Look for flights overhead
+def get_flights():
+    try:
+        # response = json.loads(matrixportal.fetch())
+        response = urequests.get(url=FLIGHT_SEARCH_URL, headers=rheaders).json()
+        print(response)
+    except:
+        print("Error during fetch")
+        return False
+    if len(response) == 3:
+        # print ("Flight found.")
+        for flight_id, flight_info in response.items():
+            # the JSON has three main fields, we want the one that's a flight ID
+            if not (flight_id == "version" or flight_id == "full_count"):
+                if len(flight_info) > 13:
+                    return flight_id
+    else:
+        return False
+
 
 def display_logo(oled):
     oled.text("Booting up ^", 0, 16 * 2)
@@ -63,8 +101,12 @@ def checkConnection():
     attempt = 1
     while (wlan.isconnected() == False) and attempt < attempts:
         print("Connect attempt " + str(attempt) + " of " + str(attempts))
-        print("Attempt WiFi connect...")
-        wlan.connect(secrets["ssid"], secrets["password"])
+        try:
+            print("Attempt WiFi connect...")
+            wlan.connect(secrets["ssid"], secrets["password"])
+        except Exception as e:
+            print(e.__class__.__name__ + "----------------ERROR---------------")
+            print(e)
         attempt += 1
     if wlan.isconnected() == True:
         print(f"Successfully connected. Status: {wlan.status()}")
@@ -87,14 +129,11 @@ display_logo(oled)
 sleep(2)
 
 checkConnection()
-print("Querying the current GMT+0 time:")
-r = urequests.get(
-    "http://date.jsontest.com"
-)  # Server that returns the current GMT+0 time.
-print(r.json())
 
 display_pikachu(oled)
 sleep(2)
+
+print(get_flights())
 
 display_plane(oled)
 
